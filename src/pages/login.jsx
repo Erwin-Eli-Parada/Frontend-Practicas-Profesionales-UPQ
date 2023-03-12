4a286ec6f80dad3f8025cb971cbc1cb521cc63e5
Eli <perwin119@gmail.com>
2023-03-12 17:49:05 -0600

    estructura login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,6 +2,13 @@ import React from "react";
 
 export function Login(props){
     return(
-        <h1>Login</h1>
+        <div className="main-container">
+            <form className="login-container">
+                <h1>Bienvenido al sistema de practicas profesionales</h1>
+                <input type="text" className="username" placeholder="nombre de usuario"/>
+                <input type="password" className="password" placeholder="contraseña"/>
+                <input type="submit" value="Login" className="submit"/>
+            </form>
+        </div>
     )
 }
